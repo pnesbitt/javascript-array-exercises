2bf727de8334baf927cdde74b795f98851ec7ba5
Paul Nesbitt <paul.nesbitt@erdosmiller.com>
2014-10-02 18:44:59 -0500

    Add Unfriend button. Had to handle the click event for those buttons in a hackish way I think.

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-var friendList = [];
+var friendList = ["Alex", "Josh", "Steven"];
 
-var updateList = function() {
-    var listElement = "<li>" + friendList[friendList.length - 1] + "</li>";
+window.onload = function() {
+    for(var i = 0; i < friendList.length; i++)
+    {
+        var listElement = "<li id='index" + i + "'>" + friendList[i] + " <button class='unfriend' value='" + i + "'>Unfriend</button></li>";
+        $("#friendList").append(listElement);
+    }
+
+    $(".unfriend").on('click', function() {
+        var index = this.value;
+        friendList.splice(index, 1);
+        removeFriend(index);
+    });
+};
+
+var addFriend = function(name) {
+    var listElement = "<li id='index" + (friendList.length - 1) + "'>" + friendList[friendList.length - 1] + " <button class='unfriend' value='" + (friendList.length - 1) + "'>Unfriend</button></li>";
     $("#friendList").append(listElement);
-}
+};
 
-$(".submitFriend").on('click', function() {
-    var name = $(".friendName").val();
+var removeFriend = function(index) {
+    $("#index" + index).remove();
+};
+
+$("#submitFriend").on('click', function() {
+    var name = $("#friendName").val();
     friendList.push(name);
-    updateList();
+    addFriend(name);
+    
+    $(".unfriend").on('click', function() {
+        var index = this.value;
+        friendList.splice(index, 1);
+        removeFriend(index);
+    });
 });
